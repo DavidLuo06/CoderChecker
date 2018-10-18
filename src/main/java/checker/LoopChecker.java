5c3c24b2324b7fc2998b86efb09dee769f6a7735
DavidLuo06 <luoyu0811@gmail.com>
2018-10-18 13:02:21 -0400

    implement another 2 checker pattern

--- src/main/java/checker/LoopChecker.java
+++ src/main/java/checker/LoopChecker.java
@@ -16,6 +16,109 @@ public class LoopChecker extends BaseChecker {
     @Override
     void check() {
 
+
+        for(ForStmt d: adapter.forStmts) {
+
+            boolean neverUse = true;
+            ArrayList<String> calResult = new ArrayList();
+            ArrayList<MethodCallExpr> methodCall= new ArrayList();
+
+            List<Node> a = d.getBody().getChildNodes();
+            for(Node dd: a) {
+                if(dd.getChildNodes().size()==0) break;
+                Node aa = dd.getChildNodes().get(0);
+
+                if(aa instanceof VariableDeclarationExpr) {
+                    NodeList<VariableDeclarator> c =
+                            ((VariableDeclarationExpr)aa).getVariables();
+
+
+                    for(int i=0; i<c.size(); i++){
+
+                        calResult.add(c.get(i).getName().toString());
+                    }
+
+                } else if (aa instanceof AssignExpr) {
+
+                    calResult.add(((AssignExpr) aa).getTarget().toString());
+                }
+                else if(aa instanceof MethodCallExpr){
+                    methodCall.add((MethodCallExpr) aa);
+                }
+            }
+            for(MethodCallExpr c: methodCall) {
+                if(checkSubNode(c.getArguments(), calResult)){
+                    neverUse = false;
+                }
+            }
+
+            if(calResult.size()!=0&&neverUse) {
+                error("[Error] Line "+d.getBody().getBegin().get().line+
+                        ": There may be unneeded computation in loops, where you call a method inside a loop, but the return value is never used.");
+            }
+        }
+
+        for(WhileStmt d: adapter.whileStmts) {
+
+            boolean neverUse = true;
+            ArrayList<String> calResult = new ArrayList();
+            ArrayList<MethodCallExpr> methodCall= new ArrayList();
+
+            List<Node> a = d.getBody().getChildNodes();
+            for(Node dd: a) {
+                if(dd.getChildNodes().size()==0) break;
+                Node aa = dd.getChildNodes().get(0);
+
+                if(aa instanceof VariableDeclarationExpr) {
+                    NodeList<VariableDeclarator> c =
+                            ((VariableDeclarationExpr)aa).getVariables();
+
+
+                    for(int i=0; i<c.size(); i++){
+
+                        calResult.add(c.get(i).getName().toString());
+                    }
+
+                } else if (aa instanceof AssignExpr) {
+
+                    calResult.add(((AssignExpr) aa).getTarget().toString());
+                }
+                else if(aa instanceof MethodCallExpr){
+                    methodCall.add((MethodCallExpr) aa);
+                }
+            }
+            for(MethodCallExpr c: methodCall) {
+                if(checkSubNode(c.getArguments(), calResult)){
+                    neverUse = false;
+                }
+            }
+
+            if(calResult.size()!=0&&neverUse) {
+                error("[Error] Line "+d.getBody().getBegin().get().line+
+                        ": There may be unneeded computation in loops, where you call a method inside a loop, but the return value is never used.");
+            }
+        }
+
+    }
+
+    private boolean checkSubNode(NodeList<Expression> e, ArrayList<String> calResult) {
+        for(int i=0; i<e.size(); i++) {
+            if(e.get(i) instanceof BinaryExpr) {
+                NodeList<Expression> ne =  new NodeList ();
+                ne.add(((BinaryExpr) e.get(i)).getLeft());
+                ne.add(((BinaryExpr) e.get(i)).getRight());
+
+                if(checkSubNode(ne, calResult)) return true;
+            } else if(e.get(i) instanceof MethodCallExpr){
+
+             if(checkSubNode(((MethodCallExpr) e.get(i)).getArguments(), calResult)) return true;
+            } else {
+
+                if(calResult.contains(e.get(i).toString())) return true;
+            }
+
+        }
+        return false;
     }
 
 }
--- src/main/java/checker/OverCatchExceptionChecker.java
+++ src/main/java/checker/OverCatchExceptionChecker.java
@@ -9,9 +9,32 @@ import java.util.List;
 
 public class OverCatchExceptionChecker extends BaseChecker {
 
+    /**
+     *  first find those over catch stmt (i.e. Exception or RuntimeException)
+     *  Then loop for those children node, if has method call named exit or
+     *  abort show error message for it;
+     * */
     @Override
     void check() {
+        for(CatchClause d: adapter.catchClause) {
 
+            List<Node> ln = d.getBody().getChildNodes();
+            for(Node n : ln) {
+                if(n instanceof ExpressionStmt) {
+                    if(n.getChildNodes().get(0) instanceof MethodCallExpr){
+                        String name = ((MethodCallExpr) n.getChildNodes().get(0)).getNameAsString();
+                        if(d.getParameter().getType().toString().equals("Exception")||
+                                d.getParameter().getType().toString().equals("RuntimeException")) {
+                            if (name.equals("abort") || name.equals("exit")) {
+                                error("[Error] Line " + n.getChildNodes().get(0).getBegin().get().line + ": Developers are over-catching an exception " +
+                                        "(i.e., catching very high-level exceptions, such as Exception or RunTimeException), and are calling abort or System.exit() in the catch block.");
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
     }
 
 }
